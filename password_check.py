d3946daf1ef6468253c9e670227ba3ad18a1fab8
Nurul Syafini <nurulsyafinisubhan.bintemohamedaidilsubhan@my.jcu.edu.au>
2022-04-02 17:53:45 +0800

    Refactor password check program to use functions

---
+++ password_check.py
@@ -0,0 +1,30 @@
+"""
+Description: A simple password checker
+
+Name: Nurul Syafini Subhan
+"""
+
+
+def is_valid_password(text):
+    """Check whether a given text has the correct password format"""
+    return 8 <= len(text) <= 20
+
+
+def main():
+    """Start program"""
+    get_password()
+
+
+def get_password():
+    new_password = input("Input password: ")
+    print_asterisk(new_password)
+
+
+def print_asterisk(new_password):
+    new_password = len(new_password)
+    for i in range(new_password):
+        print('*', end='')
+
+
+if __name__ == '__main__':
+    main()
